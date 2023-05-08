9819877cc6a5ea913b21b218bc3ebfcee61e8079
mgd666 <1291788563@qq.com>
2023-05-08 08:54:41 +0800

    modify some files architecture

--- engine/sternman_engine.py
+++ engine/sternman_engine.py
@@ -1,10 +1,12 @@
-from engine import Engine
+from engine.engine import Engine
 
 
 class SternmanEngine(Engine):
-    def __init__(self, warning_indicator_on):
-        super().__init__(0, 0, warning_indicator_on)
-        self.warning_indicator_on = warning_indicator_on
+    def __init__(self, warning_light_is_on):
+        self.warning_light_is_on = warning_light_is_on
 
-    def engine_needs_service(self):
-        return self.warning_indicator_on
+    def needs_service(self):
+        if self.warning_light_is_on:
+            return True
+        else:
+            return False
